05ae0b79e519627a152e43ab8b21d8255ec287e0
Rodion Glukhov <public.rodion@icloud.com>
2021-04-01 16:26:33 +0300

    Мелкие изменения

--- time_logger/__init__.py
+++ time_logger/__init__.py
@@ -1,8 +1,8 @@
 """Приложение для подсчёта затраченного на задачи времени."""
-__version__ = (0, 3, 1)
+__version__ = (0, 4, 0)
 __description__ = __doc__
 __author__ = "Rodion"
 __author_email__ = "public.rodion@icloud.com"
-__project_name__ = __name__.replace("_", "-")
-__url__ = F"https://github.com/rodion4dev/{__project_name__}"
-__cli_handler__ = f"{__name__}.cli:run"
+__project_name__ = __package__.replace("_", "-")
+__url__ = f"https://github.com/rodion4dev/{__project_name__}"
+__cli_handler__ = f"{__package__}.cli:run"
